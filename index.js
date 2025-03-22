eaeb69bb2d1fd10c3bdfa41b348e8ba919ef542c
Puttaraja024 <gnputtaraja06@gmail.com>
2025-03-22 23:37:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let card = document.querySelectorAll(".trend");
+let blog = document.querySelector(".trends");
+
+let mainpage = document.querySelector(".main");
+function shops(){
+    mainpage.computedStyleMap.display="none";
+    blog.computedStyleMap.display="none";
+    
+}
